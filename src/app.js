ab3c7f8322507f60823af1400ddadb7b1f6cc6a1
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-03-23 14:42:37 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excusa").innerHTML = generarExcusa();
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generarExcusa() {
+  let excusa =
+    devolverRandomInfo(who) +
+    " " +
+    devolverRandomInfo(action) +
+    " " +
+    devolverRandomInfo(what) +
+    " " +
+    devolverRandomInfo(when);
+  return excusa;
+}
+
+function devolverRandomInfo(array) {
+  return array[Math.floor(Math.random() * array.length) + 0];
+}
