29f4bc43ed02e6c4ea63280f96e1ddda8df0af80
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-03-27 11:02:53 +0000

    Cambios en la forma de realizar el ejercicio

--- src/app.js
+++ src/app.js
@@ -7,32 +7,56 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  document.getElementById("excusa").innerHTML = generarExcusa();
+  const getRandomElement = array => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  };
+
+  const getGuilty = () => {
+    const who = ["The dog", "My grandma", "The mailman", "My bird"];
+    return getRandomElement(who);
+  };
+
+  const getAction = () => {
+    const action = ["ate", "peed", "crushed", "broke"];
+    return getRandomElement(action);
+  };
+
+  const getTarget = () => {
+    const target = ["my homework", "my phone", "the car"];
+    return getRandomElement(target);
+  };
+
+  const getWhen = () => {
+    const when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+    return getRandomElement(when);
+  };
+
+  const getMessage = (guilty, action, target, when) => {
+    return `${guilty} ${action} ${target} ${when}`;
+  };
+
+  const printMessage = message => {
+    console.log(message);
+  };
+
+  function main() {
+    const guilty = getGuilty();
+    const action = getAction();
+    const target = getTarget();
+    const when = getWhen();
+
+    const message = getMessage(guilty, action, target, when);
+    printMessage(message);
+
+    document.getElementById("excusa").innerHTML = message;
+  }
+
+  main();
 };
-
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
-
-function generarExcusa() {
-  let excusa =
-    devolverRandomInfo(who) +
-    " " +
-    devolverRandomInfo(action) +
-    " " +
-    devolverRandomInfo(what) +
-    " " +
-    devolverRandomInfo(when);
-  return excusa;
-}
-
-function devolverRandomInfo(array) {
-  return array[Math.floor(Math.random() * array.length) + 0];
-}
